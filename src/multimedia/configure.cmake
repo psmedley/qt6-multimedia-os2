0b3637170da3c60d7abfcb0b4080af82b742c5ee
Paul Smedley <paul@smedley.id.au>
2025-05-01 17:54:29 +1030

    OS/2 changes
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

--- src/multimedia/configure.cmake
+++ src/multimedia/configure.cmake
@@ -138,7 +138,7 @@ qt_feature("ffmpeg" PRIVATE
     LABEL "FFmpeg"
     ENABLE INPUT_ffmpeg STREQUAL 'yes'
     DISABLE INPUT_ffmpeg STREQUAL 'no'
-    CONDITION FFmpeg_FOUND AND (APPLE OR WIN32 OR ANDROID OR QNX OR QT_FEATURE_pulseaudio)
+    CONDITION FFmpeg_FOUND AND (APPLE OR WIN32 OR ANDROID OR QNX OR OS2 OR QT_FEATURE_pulseaudio)
 )
 qt_feature("pipewire" PRIVATE
     LABEL "PipeWire"
